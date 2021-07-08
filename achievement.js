60a174868b02b582b3f0a2e17c5316fc03a469ee
mouhab-molki <87101895+mouhab-molki@users.noreply.github.com>
2021-07-08 03:05:48 +0300

    Add files via upload

---
+++ achievement.js
@@ -0,0 +1,57 @@
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelector(".nav-links li");
+
+menu.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.ForEach(link => {
+    link.classList.toggle("fade");
+  });
+     
+});
+menu.addEventListener('click' ,()=> {
+   //menu animation
+      menu.classList.toggle("toggle");
+})
+
+
+
+
+
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
